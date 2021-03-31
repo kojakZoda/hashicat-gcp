d47ebce84fbd2c2b3f8c09eee49c7890288827fe
kojakZoda <kojak@blooo.io>
2021-03-31 16:09:57 +0000

    Changed vpc name

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "terraform-google-modules/network/google"
   version = "2.5.0"
   # insert the 3 required variables here
-  network_name = "lavrèmen-vpc"
+  network_name = "lavremen-vpc"
   project_id = var.project
   subnets = [{
     subnet_name   = "gaurav-subnet"
